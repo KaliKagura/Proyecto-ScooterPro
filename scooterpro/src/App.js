52f043928b7fc7eeea358fe16cd370149bdd3c70
AntonMarchant <a.marchantrodrguez@uandresbello.edu>
2025-05-24 21:35:20 -0400

    login

--- scooterpro/src/App.js
+++ scooterpro/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import AppRouter from './mis-routers/Routers';
 import Footer from "./components/footer/Footer.jsx"
+import Login from './components/login/Login.jsx';
 
 function App() {
   return (
